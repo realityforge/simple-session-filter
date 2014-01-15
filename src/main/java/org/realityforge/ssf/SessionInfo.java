570e2ac7c725c2162d29393788aa4a87bac381aa
Peter Donald <peter@realityforge.org>
2014-01-16 08:43:58 +1100

    Add basic interface to define session

---
+++ src/main/java/org/realityforge/ssf/SessionInfo.java
@@ -0,0 +1,31 @@
+package org.realityforge.ssf;
+
+import javax.annotation.Nonnull;
+
+/**
+ * A basic interface defining a session.
+ */
+public interface SessionInfo
+{
+  /**
+   * @return an opaque ID representing session.
+   */
+  @Nonnull
+  String getSessionID();
+
+  /**
+   * @return the username for session if available.
+   */
+  @Nonnull
+  String getUsername();
+
+  /**
+   * @return the time at which session was created.
+   */
+  long getCreatedAt();
+
+  /**
+   * @return the time at which session was last accessed.
+   */
+  long getLastAccessedAt();
+}
