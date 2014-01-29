871e7bf7485eb3d05364ecb7d12b985dc759ab44
Peter Donald <peter@realityforge.org>
2014-01-29 18:18:30 +1100

    Rework SimpleSessionInfo to support a mutable sessionID in sub-classes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 ## 0.2:
+* Rework SimpleSessionInfo to support a mutable sessionID in sub-classes.
 * Rework SessionInfo to support setting of arbitrary attributes. Remove accessor for username and treat
   username as a standard attribute.
 
--- src/main/java/org/realityforge/ssf/SimpleSessionInfo.java
+++ src/main/java/org/realityforge/ssf/SimpleSessionInfo.java
@@ -9,15 +9,20 @@ import javax.annotation.Nullable;
 public class SimpleSessionInfo
   implements SessionInfo, Serializable
 {
-  private final String _sessionID;
+  private String _sessionID;
   private long _createdAt;
   private long _lastAccessedAt;
   private Map<String, Serializable> _attributes = new HashMap<>();
 
   public SimpleSessionInfo( @Nonnull final String sessionID )
   {
-    _sessionID = sessionID;
+    setSessionID( sessionID );
     _createdAt = _lastAccessedAt = System.currentTimeMillis();
+  }
+
+  protected void setSessionID( final String sessionID )
+  {
+    _sessionID = sessionID;
   }
 
   @Nonnull
